5644d23e3816ec3c21fb3475936c671aba4c74f6
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2025-03-07 20:39:33 +0100

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+  if (typeof min !== "number" ||
+      typeof max !== "number") {
+        return "ERROR";
+  }
+  if (Math.floor(min) !== min ||
+      Math.floor(max !== max)) {
+        return "ERROR";
+      }
+  if (min < 0) {
+    return "ERROR";
+  }
+  if (min > max) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
