2e70dd54437a10a014bf2f47bdca8bac0d594160
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2025-03-11 22:41:44 +0100

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const exclude = ["!", ".", " ", ","];
+  const clean = str.toLowerCase().split("").filter(char => !exclude.includes(char)).join("");
+  return clean === clean.split("").reverse().join("");
 };
 
 // Do not edit below this line
