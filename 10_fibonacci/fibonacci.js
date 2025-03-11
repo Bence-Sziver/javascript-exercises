eb169b818b2d8e2633a2af0b8e7e63b799158f76
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2025-03-11 23:07:59 +0100

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (+num < 0) return "OOPS";
+  if (+num === 0) return 0;
 
+  let prev1 = 1;
+  let prev2 = 0;
+  let fib = 1;
+  for (let i = 2; i <= +num; i++) {
+    fib = prev1 + prev2;
+    prev2 = prev1;
+    prev1 = fib;
+  }
+  return fib;
 };
 
 // Do not edit below this line
