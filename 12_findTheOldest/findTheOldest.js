3713de14bc23a51bbec7441ba829756a75e57793
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2025-03-11 23:28:05 +0100

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  return arr.reduce((obj, person) => {
+    if (obj.yearOfDeath === undefined) {
+      obj.yearOfDeath = new Date().getFullYear();
+    }
+    if (person.yearOfDeath - person.yearOfBirth > obj.yearOfDeath - obj.yearOfBirth) {
+      obj = person;
+    }
+    return obj;
+  });
 };
 
 // Do not edit below this line
